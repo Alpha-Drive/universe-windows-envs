46830d92d70e79ed0bf1e5e1597f160439bbfb26
Craig Quiter <cquiter@gmail.com>
2016-12-08 17:51:16 -0800

    spacing

--- vnc-gtav/update.py
+++ vnc-gtav/update.py
@@ -1,7 +1,6 @@
 import gym_windows
 import os
 
-# Download
-
-gym_windows.download_folder('https://www.dropbox.com/s/ljx7uiodptxr0f3/universe-windows-envs.zip?dl=1', os.environ('UNIVERSE_WINDOWS_ENVS_DIR'))
+gym_windows.download_folder('https://www.dropbox.com/s/ljx7uiodptxr0f3/universe-windows-envs.zip?dl=1',
+                            os.environ('UNIVERSE_WINDOWS_ENVS_DIR'))
 
