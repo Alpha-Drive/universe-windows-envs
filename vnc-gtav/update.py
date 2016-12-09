638c7a60535b7d0705fa457a9c338f0e300e6bb1
Craig Quiter <cquiter@gmail.com>
2016-12-08 18:02:55 -0800

    update.py fixes

--- vnc-gtav/update.py
+++ vnc-gtav/update.py
@@ -1,6 +1,8 @@
-import gym_windows
 import os
-
+import sys
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(os.path.abspath(os.path.join(CURR_DIR, os.pardir)), 'vnc-windows'))
+import gym_windows
 gym_windows.download_folder('https://www.dropbox.com/s/ljx7uiodptxr0f3/universe-windows-envs.zip?dl=1',
                             os.environ('UNIVERSE_WINDOWS_ENVS_DIR'))
 
