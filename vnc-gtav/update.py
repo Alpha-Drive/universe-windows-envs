755d42c313c1c96a85eeb5ec864e43bbf7546ec7
Craig Quiter <cquiter@gmail.com>
2016-12-08 18:06:59 -0800

    update.py logging

--- vnc-gtav/update.py
+++ vnc-gtav/update.py
@@ -1,8 +1,19 @@
 import os
 import sys
+import logging
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(os.path.abspath(os.path.join(CURR_DIR, os.pardir)), 'vnc-windows'))
 import gym_windows
-gym_windows.download_folder('https://www.dropbox.com/s/ljx7uiodptxr0f3/universe-windows-envs.zip?dl=1',
-                            os.environ('UNIVERSE_WINDOWS_ENVS_DIR'))
 
+logger = logging.getLogger()
+
+def main():
+    gym_windows.download_folder('https://www.dropbox.com/s/ljx7uiodptxr0f3/universe-windows-envs.zip?dl=1',
+                                os.environ['UNIVERSE_WINDOWS_ENVS_DIR'])
+    logger.info('Update complete')
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+    sys.exit(main())
